536c4ce756a90bcb48ec20012bbde459dc43b4c4
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-07 17:53:44 -0800

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-last-element.js
@@ -0,0 +1,6 @@
+/* exported getLastElement */
+function getLastElement(array) {
+  var getLastElementIndex = array.length - 1;
+  var lastElement = array[getLastElementIndex];
+  return lastElement;
+}
