633137026f1896afa07a4d36ecbcfd8a168d77a9
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-16 14:56:08 -0800

    Saved changes on string-manipulation-3 algorithms.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,4 +1,8 @@
 /* exported reverseWords */
+/* - Split each group of words into one word and reverse the individual letters of word
+- Put the word back together and apply the spacing between the words and bring them back together
+- Return that value as your output */
+
 function reverseWords(string) {
   return string.split('').reverse().join('').split(' ').reverse().join(' ');
 }
