f324cb437d1b976edae8cb7a9bfaff66178d70a4
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-17 15:16:22 -0800

    Revised number to Number for stringify in js.

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -33,7 +33,7 @@ console.log('typeof thirdBook:', typeof thirdBook);
 console.log('Array of books:', arrayOfBooks);
 console.log('typeof arrayOfBooks:', typeof arrayOfBooks);
 
-var stringOfStudent = '{"name": "string", "id": "number"}';
+var stringOfStudent = '{"name": "string", "id": "Number"}';
 console.log('stringOfStudent:', stringOfStudent);
 console.log('typeof stringOfStudent:', typeof stringOfStudent);
 
