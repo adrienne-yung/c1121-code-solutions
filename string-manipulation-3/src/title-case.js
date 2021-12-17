f4cbbf64406ad6796f49a3c698a7791b9bc0001e
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-17 15:12:30 -0800

    made progress on titlecase.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,17 @@
 /* exported titleCase */
+function titleCase(title) {
+  title = title.toLowerCase().split(' ');
+  var lowerCaseWords = ['In', 'For', 'Of', 'The', 'To', 'On', 'And'];
+  for (var i = 0; i < title.length; i++) {
+    title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+    if (title[i] === 'Api') {
+      title[i] = title[i].toUpperCase();
+    }
+    for (var e = 0; e < lowerCaseWords.length; e++) {
+      if ((lowerCaseWords[e] !== title[i].charAt(0)) && (lowerCaseWords[e] === lowerCaseWords)) {
+        title[e] = title[e].toLowerCase;
+      }
+    }
+  }
+  return title.join(' ');
+}
