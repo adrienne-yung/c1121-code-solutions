ef5aaea18119b9b39703e6a2526eab912e77a364
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-17 15:13:18 -0800

    Saved changes to titlecase.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,4 +1,13 @@
 /* exported titleCase */
+
+/* - Create a storage space to hold the title and lowercase everything in the title, then split the phrase into individual words
+- Create a storage space to hold the value of the words that need to lowercased
+- Traverse through the first storage space to uppercase every letter that is the first letter of the word
+- In the situation that the word is "Api", capitalize the all letters into "API", since it's an acronym
+- Put back together the words into the phrase
+- In the situation that word is a lowercased word AND is not the first word of the title, lowercase the whole word and store
+- Return the value of the storage space as your output */
+
 function titleCase(title) {
   title = title.toLowerCase().split(' ');
   var lowerCaseWords = ['In', 'For', 'Of', 'The', 'To', 'On', 'And'];
