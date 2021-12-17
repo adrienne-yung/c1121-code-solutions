a6aad1278177243c86cc953ade15b9d474330354
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-17 15:55:49 -0800

    Added more conditionals to string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -10,16 +10,23 @@
 
 function titleCase(title) {
   title = title.toLowerCase().split(' ');
-  var lowerCaseWords = ['In', 'For', 'Of', 'The', 'To', 'On', 'And'];
+  // var lowerCaseWords = ['In', 'For', 'Of', 'The', 'To', 'On', 'And'];
   for (var i = 0; i < title.length; i++) {
     title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
     if (title[i] === 'Api') {
       title[i] = title[i].toUpperCase();
     }
-    for (var e = 0; e < lowerCaseWords.length; e++) {
-      if ((lowerCaseWords[e] !== title[i].charAt(0)) && (lowerCaseWords[e] === lowerCaseWords)) {
-        title[e] = title[e].toLowerCase;
-      }
+    if ((title[i] === 'In') || (title[i] === 'For') || (title[i] === 'Of') || (title[i] === 'The') || (title[i] === 'To') || (title[i] === 'On') || (title[i] === 'And')) {
+      title[i] = title[i].toLowerCase();
+    }
+    if (title[i] === 'Javascript') {
+      title[i] = 'JavaScript';
+    }
+    if (title[i] === 'Javascript:') {
+      title[i] = 'JavaScript:';
+    }
+    if (title[i] === 'In-depth') {
+      title[i] = 'In-Depth';
     }
   }
   return title.join(' ');
