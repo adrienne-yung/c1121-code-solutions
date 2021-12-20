2cc7c69ca89e25ade9543534634144f498946be8
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-20 10:03:40 -0800

    Worked on string-manipulation-3.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -8,28 +8,27 @@
 - In the situation that word is a lowercased word AND is not the first word of the title, lowercase the whole word and store
 - Return the value of the storage space as your output */
 
-function titleCase(title) {
-  title = title.toLowerCase().split(' ');
-  // var lowerCaseWords = ['In', 'For', 'Of', 'The', 'To', 'On', 'And'];
-  for (var i = 0; i < title.length; i++) {
-    title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
-    if (title[i] === 'Api') {
-      title[i] = title[i].toUpperCase();
-    }
-    if (title[i] === 'Javascript') {
-      title[i] = 'JavaScript';
-    }
-    if (title[i] === 'Javascript:') {
-      title[i] = 'JavaScript:';
-    }
-    if (title[i] === 'In-depth') {
-      title[i] = 'In-Depth';
-    }
-    if ((title[i] === 'In') || (title[i] === 'For') || (title[i] === 'Of') || (title[i] === 'The') || (title[i] === 'To') || (title[i] === 'On') || (title[i] === 'And')) {
-      title[i] = title[i].toLowerCase();
-    }
-    title.join(' ');
-    title.split(': ');
-  }
-  return title.join(' ');
-}
+// function titleCase(title) {
+//   title = title.toLowerCase().split(' ');
+//   for (var i = 0; i < title.length; i++) {
+//     title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+//     if (title[i] === 'Api') {
+//       title[i] = title[i].toUpperCase();
+//     }
+//     if (title[i] === 'Javascript') {
+//       title[i] = 'JavaScript';
+//     }
+//     if (title[i] === 'Javascript:') {
+//       title[i] = 'JavaScript:';
+//     }
+//     if (title[i] === 'In-depth') {
+//       title[i] = 'In-Depth';
+//     }
+//     if ((title[i] === 'In') || (title[i] === 'For') || (title[i] === 'Of') || (title[i] === 'The') || (title[i] === 'To') || (title[i] === 'On') || (title[i] === 'And')) {
+//       title[i] = title[i].toLowerCase();
+//     }
+//     if (title[i].includes(':')) {
+//       title[i + 1].charAt(0) = title[i + 1].charAt(0).toUpperCase() title[i + 1].slice(1).toLowerCase();
+//     }
+//   return title.join(' ');
+// }
