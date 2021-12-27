912bde3231634b238b476514217726c18cdfa08c
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-27 12:58:07 -0800

    Completed javascript-custom-methods exercise.

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,39 @@
+/* exported calculator */
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+
+  subtract: function (x, y) {
+    return x - y;
+  },
+
+  multiply: function (x, y) {
+    return x * y;
+  },
+
+  divide: function (x, y) {
+    return x / y;
+  },
+
+  square: function (x) {
+    return x ** 2;
+  },
+
+  sumAll: function (numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+
+  getAverage(numbers) {
+    var sumOfNumbers = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sumOfNumbers += numbers[i];
+    }
+    return sumOfNumbers / numbers.length;
+  }
+
+};
