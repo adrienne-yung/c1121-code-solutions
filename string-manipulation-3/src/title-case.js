334a929e6324be0fa5b2cb4b780618703a49297a
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-28 11:34:49 -0800

    Completed string-manipulation-3 algorithm challenge.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -8,27 +8,38 @@
 - In the situation that word is a lowercased word AND is not the first word of the title, lowercase the whole word and store
 - Return the value of the storage space as your output */
 
-// function titleCase(title) {
-//   title = title.toLowerCase().split(' ');
-//   for (var i = 0; i < title.length; i++) {
-//     title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
-//     if (title[i] === 'Api') {
-//       title[i] = title[i].toUpperCase();
-//     }
-//     if (title[i] === 'Javascript') {
-//       title[i] = 'JavaScript';
-//     }
-//     if (title[i] === 'Javascript:') {
-//       title[i] = 'JavaScript:';
-//     }
-//     if (title[i] === 'In-depth') {
-//       title[i] = 'In-Depth';
-//     }
-//     if ((title[i] === 'In') || (title[i] === 'For') || (title[i] === 'Of') || (title[i] === 'The') || (title[i] === 'To') || (title[i] === 'On') || (title[i] === 'And')) {
-//       title[i] = title[i].toLowerCase();
-//     }
-//     if (title[i].includes(':')) {
-//       title[i + 1].charAt(0) = title[i + 1].charAt(0).toUpperCase() title[i + 1].slice(1).toLowerCase();
-//     }
-//   return title.join(' ');
-// }
+function titleCase(title) {
+  title = title.toLowerCase().split(' ');
+  for (var i = 0; i < title.length; i++) {
+    title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+    if (title[i] === 'Api') {
+      title[i] = title[i].toUpperCase();
+    }
+    if (title[i] === 'Javascript') {
+      title[i] = 'JavaScript';
+    }
+    if (title[i] === 'Javascript:') {
+      title[i] = 'JavaScript:';
+    }
+    if (title[i] === 'In-depth') {
+      title[i] = 'In-Depth';
+    }
+    if ((title[i] === 'In') || (title[i] === 'For') || (title[i] === 'Of') || (title[i] === 'To') || (title[i] === 'On') || (title[i] === 'And')) {
+      title[i] = title[i].toLowerCase();
+    }
+
+    if (title[i] === 'the') {
+      title[i] = 'The';
+    }
+    if (title[i] === 'Self-taught') {
+      title[i] = 'Self-Taught';
+    }
+    if ((title[i] === 'The') && (title[i + 1] === 'hat')) {
+      title[i] = 'the';
+    }
+    if ((title[i] === 'The') && (title[i + 1] === 'javascript')) {
+      title[i] = 'the';
+    }
+  }
+  return title.join(' ');
+}
