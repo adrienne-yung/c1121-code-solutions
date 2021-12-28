9e489445b87834dee4fbc73fe77ddc6db238b0fa
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-28 11:40:40 -0800

    Added pseudo-code to isAnagram algorithm.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -10,6 +10,16 @@
 // //   }
 // }
 
+// - Create a storage space to hold the value of your first group of words
+// - Create a storage space to hold the value of your second group of words
+// - Traverse through the first group of words
+// - In the condition the character is not a space, add character into storage space for first group of words
+// - Traverse through teh second group of words
+// - In the condition the character is not a space, add character into storage space for second group of words
+// - Sort the words characters in both storage spaces and join them
+// - If the value of the first storage space doesn't equal the value of the second storage space, return false
+// - Or else return true
+
 function isAnagram(firstString, secondString) {
   var firstStringItem = [];
   var secondStringItem = [];
