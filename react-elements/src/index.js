171c42ff043bf0e6177a64f37d60283bd1b04fb2
Adrienne Yung <adrienneyung64@gmail.com>
2022-02-01 15:45:02 -0800

    Completed react-elements exercise.

---
+++ react-elements/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const greeting = React.createElement('h1', null, 'Hello, React!');
+ReactDOM.render(greeting, document.getElementById('root'));
