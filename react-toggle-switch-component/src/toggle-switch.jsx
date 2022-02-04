8f7575c9fa38c8aa3aaaaadea1b997412acc7dba
Adrienne Yung <adrienneyung64@gmail.com>
2022-02-03 16:19:25 -0800

    Completed react-toggle-switch-component challenge.

---
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: !this.state.isClicked });
+  }
+
+  render() {
+    const clicked = this.state.isClicked;
+    return (
+      <div id='react-toggle-switch'>
+        <div className={!clicked ? 'switch background' : 'switch background-active'}></div>
+        <div onClick={this.handleClick} className={!clicked ? 'slide slide-direction' : 'slide slide-direction-active'}></div>
+        <label>{!clicked ? 'OFF' : 'ON'}</label>
+      </div>
+    );
+  }
+}
+
+export default ToggleSwitch;
